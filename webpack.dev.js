67172b01e6d54ab694fa970ac64fe4541f3304cc
Roland Abregorivas <abregorivas@gmail.com>
2018-02-15 01:57:57 -0800

    feat: basic page-layout

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,18 @@
-const path = require("path");
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
-const PurifyCSSPlugin = require("purifycss-webpack");
-const glob = require("glob");
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const glob = require('glob');
 
 module.exports = merge(common, {
   output: {
-    filename: "[name].[hash].js",
-    path: path.resolve(__dirname, "dist")
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   plugins: [
     new PurifyCSSPlugin({
-      paths: glob.sync(path.join(__dirname, "src/*.html")),
+      paths: glob.sync(path.join(__dirname, 'src/*.html')),
       minimize: false,
       purifyOptions: {
         whitelist: []
