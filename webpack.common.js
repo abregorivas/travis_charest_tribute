37bcb2c33704f4fcb02f7e3d09f29c7459d009b1
Roland Abregorivas <abregorivas@gmail.com>
2018-02-14 21:22:44 -0800

    config: webpack added

---
+++ webpack.common.js
@@ -0,0 +1,107 @@
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ['./src/index.js', './src/scss/main.scss']
+    // vendor: ['jQuery']
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.css|\.s[ac]ss)$/,
+        exclude: /node_modules/,
+        use: ['css-hot-loader'].concat(
+          ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: false,
+                  sourceMap: true,
+                  importLoaders: 2
+                  // localIdentName: '[name]__[local]___[hash:base64:5]'
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  config: {
+                    path: './postcss.config.js'
+                  },
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        )
+      },
+      {
+        test: /\.(svg|ttf|woff|woff2)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        loaders: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'images/',
+              publicPath: '/'
+            }
+          },
+          'img-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+      {
+        test: /\.html$/,
+        loaders: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ],
+        exclude: path.resolve(__dirname, 'src/index.html')
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loaders: [
+          {
+            loader: 'standard-loader',
+            options: {
+              error: false,
+              snazzy: true,
+              parser: 'babel-eslint'
+            }
+          }
+        ],
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist/*.*'], { root: __dirname, verbose: true, dry: false }),
+    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }),
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new ExtractTextPlugin({ filename: '[name].css', disable: false, allChunks: true }),
+    new webpack.optimize.CommonsChunkPlugin({ names: ['vendor', 'manifest'] })
+  ]
+};
---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const PurifyCSSPlugin = require("purifycss-webpack");
+const glob = require("glob");
+
+module.exports = merge(common, {
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new PurifyCSSPlugin({
+      paths: glob.sync(path.join(__dirname, "src/*.html")),
+      minimize: false,
+      purifyOptions: {
+        whitelist: []
+      }
+    })
+  ]
+});
